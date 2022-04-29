d75e0c853c77f033544fc5478f4b63ae6e7ce544
Abhinav Maurya <xabhi7549@gmail.com>
2022-04-29 19:26:39 +0530

    Init

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Title from './FrontPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Title />
   </React.StrictMode>
 );
 
